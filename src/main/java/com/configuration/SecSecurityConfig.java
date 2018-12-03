0e227faa640508c0bedab511313617927bf2fcf9
oguzhanonder91 <oguzhanonder75@hotmail.com>
2018-12-03 17:59:51 +0300

    added new features

--- src/main/java/com/configuration/SecSecurityConfig.java
+++ src/main/java/com/configuration/SecSecurityConfig.java
@@ -74,7 +74,7 @@ public class SecSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    public void configure( WebSecurity web) throws Exception {
+    public void configure( WebSecurity web) {
         web.ignoring().antMatchers("/resources/**");
     }
 
